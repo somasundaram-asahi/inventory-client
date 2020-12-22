ade9996279d79fcd1f99f709837f57ced5dcd5e9
samuel-asahi <63997841+samuel-asahi@users.noreply.github.com>
2020-12-22 19:59:27 +0530

    ui changes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import './App.css';
-import {BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
-import MainPage from './screens/main-page';
-import ScreenOne from './screens/screen1';
-
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import StockRequestPage from './screens/stockrequest';
+import RequestDetails from './screens/requestdetails';
+import LoginPage from './screens/login';
+import Dashboard from './screens/dashboard';
+import Nav from './screens/nav';
+import ReviewTable from './screens/review';
+import StatusDetails from './screens/statusdetails';
 
 function App() {
   return (
     <Router>
     <Switch>
-      <Route path="/" exact component ={MainPage} />
-      <Route path="/screenone" exact component ={ScreenOne} />
+      <Route path="/" exact component ={Dashboard} />
+      <Nav path="/stockrequest" exact component ={StockRequestPage} />
+      <Nav path="/request" exact component ={RequestDetails} />
+      <Nav path="/login" exact component ={LoginPage} />
+      <Nav path="/reviewtable" exact component ={ReviewTable} />
+      <Nav path="/statusdetails" exact component ={StatusDetails} />
     </Switch>
     </Router>
   );
